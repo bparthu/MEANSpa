85432f4f74bec45ea1e69b142c8fcf796ee9bb14
Parthasarathy <b.paarthu@gmail.com>
2016-11-24 19:38:45 +0530

    Backend boilerplate

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ var methodOverride = require('method-override');
     
 // config files
 var db = require('./config/db');
+var mongoose = require('mongoose');
 
 // set our port
 var port = process.env.PORT || 8080; 
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
+ mongoose.connect(db.url); 
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
